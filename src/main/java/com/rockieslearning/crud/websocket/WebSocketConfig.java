d04933bd6a66908692d638727a1fee7de94433a1
n17dccn151 <n17dccn151@student.ptithcm.edu.vn>
2021-08-15 20:56:04 +0700

    update rp

--- src/main/java/com/rockieslearning/crud/websocket/WebSocketConfig.java
+++ src/main/java/com/rockieslearning/crud/websocket/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:3000").withSockJS().setInterceptors(handshakeInterceptor);;
+		registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS().setInterceptors(handshakeInterceptor);;
 	}
 
 }
