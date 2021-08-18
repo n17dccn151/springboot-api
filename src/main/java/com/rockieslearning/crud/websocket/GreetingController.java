a094bdcafd7c7698ff754cbe97e0b14b06505b1b
n17dccn151 <n17dccn151@student.ptithcm.edu.vn>
2021-08-18 16:33:48 +0700

    update rp

--- src/main/java/com/rockieslearning/crud/websocket/GreetingController.java
+++ src/main/java/com/rockieslearning/crud/websocket/GreetingController.java
@@ -3,10 +3,12 @@ package com.rockieslearning.crud.websocket;
 import com.google.gson.Gson;
 import com.rockieslearning.crud.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.persistence.EntityManager;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
+
 @Controller
 public class GreetingController {
 
@@ -26,8 +28,20 @@ public class GreetingController {
 	@Autowired
 	private EntityManager entityManager;
 
+	private final SimpMessagingTemplate simpMessagingTemplate;
+
+	public GreetingController(SimpMessagingTemplate simpMessagingTemplate) {
+		this.simpMessagingTemplate = simpMessagingTemplate;
+	}
 
 
+	@MessageMapping("/sendMessage/{userId}")
+	public void sendMessage(@Payload MessageDto message,
+								@DestinationVariable long userId){
+		System.out.println("received: "+ message.toString());
+		simpMessagingTemplate.convertAndSend("/reciveMessage/"+userId, message);
+
+	}
 
 
 	@MessageMapping("/hello")
@@ -41,22 +55,11 @@ public class GreetingController {
 	@MessageMapping("/hi")
 	@SendTo("/topic/dasboards")
 	public Greeting testDashboard(HelloMessage message) throws Exception {
-//		List<OrderDto> orderDtos = new ArrayList<>();
-//		orderDtos = orderService.retrieveOrders();
-//		Thread.sleep(1000); // simulated delay
-//
-//		String json = new Gson().toJson(orderDtos);
-
-
 		List<Object> objects = new ArrayList<>();
 		Query query = entityManager.createNativeQuery("select f.food_name, subR.sum, subR.my_rank from ( select *, rank() over ( order by sub.sum desc) as my_rank from ( select food_id , sum(amount) from order_foods group by food_id )as sub) as subR , foods f where f.food_id = subR.food_id and subR.my_rank <= 10 order by subR.my_rank\n" );
 		objects = query.getResultList();
 		Thread.sleep(1000); // simulated delay
-
 		String json = new Gson().toJson(objects);
-
-
-
 
 		return new Greeting( json);
 	}
---
+++ src/main/java/com/rockieslearning/crud/websocket/MessageDto.java
@@ -0,0 +1,19 @@
+package com.rockieslearning.crud.websocket;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by TanVOD on Aug, 2021
+ */
+
+
+@Getter
+@Setter
+@ToString
+public class MessageDto {
+    String name;
+    String message;
+    String urlImage;
+}
